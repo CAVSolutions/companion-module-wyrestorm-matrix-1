bdb768f7048add75ed56c651b28fcccf89dfec53
Joseph Adams <josephdadams@gmail.com>
2025-10-27 13:33:35 -0400

    change to just line feed

--- src/api.ts
+++ src/api.ts
@@ -22,7 +22,7 @@ async function executeCommand(self: WyrestormMatrixInstance, command: string): P
 		if (self.config.verbose) {
 			self.log('debug', `Sending command: ${command}`)
 		}
-		self.socket?.send(command + '\r\n')
+		self.socket?.send(command + '\n')
 		self.lastCommand = command
 	} else {
 		self.log('warn', 'Not connected to Device')
